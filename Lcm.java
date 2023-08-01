bd17a13e10dec252043b96092f58255d4a03b1e6
iamsanjuoffl <iamsanju360@gmail.com>
2023-08-01 22:46:41 +0530

    Program to find LCM of two numbers

---
+++ Lcm.java
@@ -0,0 +1,16 @@
+package Java_Programming;
+import java.util.*;
+public class Lcm {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt(),n=sc.nextInt();
+        int a=(m>n)?m:n;
+        while(true){
+            if(a%n==0 && a%m==0){
+                System.out.print(a);
+                break;
+            }
+            a++;
+        }
+    }
+}
