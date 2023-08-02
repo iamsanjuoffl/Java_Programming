493365fccb9259194e6aabd10b8841dc3764e725
iamsanjuoffl <iamsanju360@gmail.com>
2023-08-02 22:19:09 +0530

    File name Changed

---
+++ Reverse_string.java
@@ -0,0 +1,12 @@
+//Reverse a String by swapping in the same Character Array {Leet Code}
+class Solution {
+    public void reverseString(char[] s) {
+        char temp;
+        for(int i=0,j=s.length-1;i<s.length/2 && j>=s.length/2;i++,j--){
+                temp=s[i];
+                s[i]=s[j];
+                s[j]=temp;
+
+        }
+    }
+}
