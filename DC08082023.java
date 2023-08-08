4cd94609ce621689e8417a869658ba22fa699cf2
iamsanjuoffl <iamsanju360@gmail.com>
2023-08-08 09:29:04 +0530

    Solved

---
+++ DC08082023.java
@@ -0,0 +1,12 @@
+package Java_Programming;
+import java.util.*;
+public class DC08082023 {
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt(),y=sc.nextInt();
+		int a=(x>y)?x:y;
+		for(int i=1;i<=10;i++){
+		    System.out.print(i*a+" ");
+		}
+	}
+}
