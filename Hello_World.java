be2c13623c53b72e08729c1d033d30c7c69ac3dc
iamsanjuoffl <iamsanju360@gmail.com>
2023-08-13 22:17:25 +0530

    Printed

--- Hello_World.java
+++ Hello_World.java
@@ -2,6 +2,8 @@
 package Java_Programming;
 public class Hello_World {
     public static void main(String[]args){
+        int a=0;
+        if(a==0)
         System.out.print("Hello World!");
     }
 }
