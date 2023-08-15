b425709c4fd2ccd2f10fa9ce4a147249252eaabe
iamsanjuoffl <iamsanju360@gmail.com>
2023-08-15 15:29:54 +0530

    solved

---
+++ DC15082023.java
@@ -0,0 +1,16 @@
+package Java_Programming;
+import java.util.*;
+public class DC15082023 {
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next(),s1="1234567890aeiouAEIOU";
+		for(int i=0;i<s.length();i++){
+		    if(s1.contains(s.charAt(i)+"")){
+		        System.out.print("+");
+		    }else{
+		        System.out.print(s.charAt(i));
+		    }
+		}
+
+	}
+}
